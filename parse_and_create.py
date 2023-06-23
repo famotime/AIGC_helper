8b551a9d63c9fd699e311c5e815519d5576f2e42
Quincy Zou <52997216+famotime@users.noreply.github.com>
2023-06-23 10:36:25 +0800

    增加异常处理

--- parse_and_create.py
+++ parse_and_create.py
@@ -13,13 +13,14 @@ def parse_parameters(data, add_prompts=None, del_prompts=None, rep_prompts=None,
     """解析civitai网站图像或本地图片文件生成参数"""
     # API生成参数key值
     keys = ['enable_hr', 'denoising_strength', 'firstphase_width', 'firstphase_height', 'hr_scale', 'hr_upscaler', 'hr_second_pass_steps', 'hr_resize_x', 'hr_resize_y', 'hr_sampler_name', 'hr_prompt', 'hr_negative_prompt', 'prompt', 'styles', 'seed', 'subseed', 'subseed_strength', 'seed_resize_from_h', 'seed_resize_from_w', 'sampler_name', 'batch_size', 'n_iter', 'steps', 'cfg_scale', 'width', 'height', 'restore_faces', 'tiling', 'do_not_save_samples', 'do_not_save_grid', 'negative_prompt', 'eta', 's_min_uncond', 's_churn', 's_tmax', 's_tmin', 's_noise', 'override_settings', 'override_settings_restore_afterwards', 'script_args', 'sampler_index', 'script_name', 'send_images', 'save_images', 'alwayson_scripts']
+    keys += ['model_hash', 'model']    # 其他需要解析的图像信息，非API参数
 
     # 生成参数与API关键字映射关系
     mappings = {'sampler': 'sampler_index', 'hires_steps': 'hr_second_pass_steps', 'hires_upscale': 'hr_scale', 'hires_upscaler': 'hr_upscaler', 'face_restoration': 'restore_faces'}
 
     # 从图片文件读取配置信息
-    if data.endswith(".png"):
-        data = get_parameters_from_img(data)
+    if str(data).endswith(".png"):
+        data = get_parameters_from_img(str(data))
 
     parameters = {}
     text = data.replace('\r', '')
@@ -28,7 +29,15 @@ def parse_parameters(data, add_prompts=None, del_prompts=None, rep_prompts=None,
         for k, v in rep_prompts.items():
             text = text.replace(k, v)
 
-    prompt, negtive_prompt, others = re.search(r'(.*?)\nNegative prompt: (.*?)\n(.*)', text, flags=re.DOTALL).groups()
+    # 解析文本信息
+    if "Negative prompt" in text:
+        prompt, negtive_prompt, others_first_key, others = re.search(r'(.*?)Negative prompt: (.*?)(ENSD|Size|Steps)(.*)', text, flags=re.DOTALL).groups()
+    else:
+        prompt, others_first_key, others = re.search(r'(.*?)(ENSD|Size|Steps)(.*)', text, flags=re.DOTALL).groups()
+        negtive_prompt = ''
+    others = others_first_key + others
+    others = others.split('\n')[0]
+    # print(prompt, negtive_prompt, others, sep='\n')
 
     # 增加或删除提示词
     if add_prompts:
@@ -38,11 +47,13 @@ def parse_parameters(data, add_prompts=None, del_prompts=None, rep_prompts=None,
             prompt = prompt.replace(i, '')
 
     parameters['prompt'] = prompt
-    parameters['negative_prompt'] = negtive_prompt
-    others = others.split('\n')[0]
+    if negtive_prompt:
+        parameters['negative_prompt'] = negtive_prompt
+    # print(others)
     for i in others.split(', '):
         k, v = i.split(': ')
         k = k.lower().replace(' ', '_')
+        # print(k, v)
         if k == 'size':
             parameters['width'], parameters['height'] = [int(i) for i in v.split('x')]
             continue
@@ -62,9 +73,9 @@ def parse_parameters(data, add_prompts=None, del_prompts=None, rep_prompts=None,
         parameters['restore_faces'] = True
 
     hr_upscalers = ['Latent', 'Latent (antialiased)', 'Latent(bicubic)', 'Latent(bicubic antialiased)', 'Latent (nearest)', 'Latent(nearest-exact)', 'None', 'Lanczos', 'Nearest', '4x-UltraSharp', 'BSRGAN', 'ESRGAN 4x', 'LDSR', 'R-ESRGAN 4x+', 'R-ESRGAN 4x+Anime6B', 'ScuNET', 'ScuNET PSNR', 'SwinlR_4x']
-    if 'hr_scale' in parameters:
+    if 'hr_upscaler' in parameters:
         parameters['enable_hr'] = True
-        if not parameters['hr_upscaler'] in hr_upscalers:
+        if parameters['hr_upscaler'] not in hr_upscalers:
             parameters['hr_upscaler'] = 'R-ESRGAN 4x+'
 
     # 删除不需要的参数
