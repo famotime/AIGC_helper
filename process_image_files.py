6203a82888828edc7ddfb5b7ce4b09db469b46df
Quincy Zou <52997216+famotime@users.noreply.github.com>
2023-07-01 15:31:19 +0800

    增加提示信息

--- process_image_files.py
+++ process_image_files.py
@@ -4,36 +4,46 @@ import parse_and_create
 from PIL import Image, PngImagePlugin
 
 
-def organize_images_by_model(folder):
+def count_files(func):
+    def wrapper(folder):
+        count = 0
+        for image in folder.glob("*.png"):
+            count += 1
+            print(f"正在处理：{image.name}...")
+            func(image)
+        print(f"\n总计处理{count}个图片文件。\n")
+    return wrapper
+
+
+@count_files
+def organize_image_by_model(image):
     """解析图片文件中包含的生成模型信息，分类归档图片文件"""
-    for image in folder.glob("*.png"):
-        print(f"Processing {image.name}...")
-        parameters = parse_and_create.parse_parameters(image)
-        # print(parameters)
-
-        model = parameters['model'] if 'model' in parameters else parameters['model_hash']
-        new_path = folder / model / image.name
-        new_path.parent.mkdir(parents=True, exist_ok=True)
-        image.rename(new_path)
+    parameters = parse_and_create.parse_parameters(image)
+    # print(parameters)
+    model = parameters['model'] if 'model' in parameters else parameters['model_hash']
+    new_path = image.parent / model / image.name
+    new_path.parent.mkdir(parents=True, exist_ok=True)
+    image.rename(new_path)
 
 
-def remove_chunk_info(input_file, output_file, chunk_name="parameters"):
+@count_files
+def remove_chunk_info(input_file):
     """清除SD WebUI生成图片文件中自动添加的生成参数信息，避免prompt等参数泄漏"""
     image = Image.open(input_file)
     png_info = PngImagePlugin.PngInfo()
+    chunk_name = "parameters"
     for key in image.info.keys():
         if key != chunk_name:
             png_info.add_text(key, image.info[key])
+    output_file = input_file.with_name(f"{input_file.stem}_no_parameters.png")
     image.save(output_file, pnginfo=png_info)
 
 
 if __name__ == "__main__":
-    folder = pathlib.Path(r"D:\sd-webui\outputs\txt2img-images\test")
+    folder = pathlib.Path(r"D:\sd-webui\outputs\txt2img-images")
 
     # 根据图片文件中的生成模型信息，分类归档图片文件
-    # organize_images_by_model(folder)
+    organize_image_by_model(folder)
 
-    # 图片文件中包含的生成参数信息，避免prompt等参数泄漏
-    for image in folder.glob("*.png"):
-        output_file = image.with_name(f"{image.stem}_no_parameters.png")
-        remove_chunk_info(image, output_file)
+    # 清除图片文件中包含的生成参数信息，避免prompt等参数泄漏
+    # remove_chunk_info(folder)
